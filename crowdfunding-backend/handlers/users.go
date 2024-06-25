aa6dc04df976fe1badaa7145edd398c516d3fc22
Alfahan <ali.farhan160@gmail.com>
2024-06-25 12:24:31 +0700

    fix: responses-error

--- crowdfunding-backend/handlers/users.go
+++ crowdfunding-backend/handlers/users.go
@@ -21,14 +21,20 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 
 	err := c.ShouldBindJSON(&input)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, nil)
+		response := helpers.APIResponse("Register account failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
 	}
 
 	newUser, err := h.userService.RegisterUser(input)
 
 	if err != nil {
-		c.JSON(http.StatusBadRequest, nil)
+		response := helpers.APIResponse("Register account failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
 	}
+
+	//token
 
 	formatter := users.FormatUsers(newUser, "tokentokentoken")
 
